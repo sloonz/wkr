dc1cef5a618a7397443e44ccfd0d1ad5b83c64e4
Simon Lipp <sloonz@gmail.com>
2014-02-14 11:28:20 +0100

    GreaseMonky script improvements
    * add a menu entry to change autofiller url
    * the iframe now takes all screen
    * allow login to be filled in a email input type
    * send hinted creds

--- autofill.user.js
+++ autofill.user.js
@@ -5,52 +5,79 @@
 // @version     1.0
 // @grant       GM_getValue
 // @grant       GM_setValue
+// @grant       GM_registerMenuCommand
 // ==/UserScript==
 
 (function() {
-	var URL = GM_getValue("url");
-	if(!URL) {
-		URL = prompt('Autofiller URL');
-		GM_setValue("url", URL);
-	}
-
-	var URL_a = document.createElement("a");
-	URL_a.href = URL;
-	ORIGIN = URL_a.protocol + "//" + URL_a.host;
-
-	if(!URL || document.location.protocol + "//" + document.location.host == ORIGIN)
-		return;
-
-	var activeElement = document.activeElement;
-	var frame = document.createElement("iframe");
-
-	frame.addEventListener("load", function() {
-		frame.contentWindow.postMessage({"action": "getCredentials", "arguments": []}, URL);
-	}, false);
-
-	var handlers = {}
-
-	handlers.onMessage = function(event) {
-		if(event.origin == ORIGIN) {
-			handlers[event.data.action].apply(window, event.data.arguments);
-		}
-		else if(event.data == "wkr_findCredentials") {
-			frame.src = URL + "?" + encodeURIComponent(window.location.toString());
-			frame.style.zIndex = 1000;
-			frame.style.position = "fixed";
-			frame.style.display = "none";
-			frame.style.border = "none";
-
-			document.body.appendChild(frame);
+	var autofillerUrl = GM_getValue("url");
+	var promptURL = function() {
+		var url = prompt('Autofiller URL', autofillerUrl);
+		if(url) {
+			autofillerUrl = url;
+			GM_setValue("url", autofillerUrl);
 		}
 	};
 
-	handlers.show = function() {
-		frame.style.display = "block";
-		frame.style.width = Math.floor(window.innerWidth*0.9) + "px";
-		frame.style.height = Math.floor(window.innerHeight*0.9) + "px";
-		frame.style.left = Math.floor(window.innerWidth*0.05) + "px";
-		frame.style.top = Math.floor(window.innerHeight*0.05) + "px";
+	GM_registerMenuCommand("Set address", promptURL);
+
+	if(!autofillerUrl) {
+		promptURL();
+	}
+
+	var autofillerUrlParsed = document.createElement("a");
+	autofillerUrlParsed.href = autofillerUrl;
+	var origin = autofillerUrlParsed.protocol + "//" + autofillerUrlParsed.host;
+
+	if(!autofillerUrl || document.location.protocol + "//" + document.location.host == origin)
+		return;
+
+	var frame = document.createElement("iframe");
+	var activeElement = document.activeElement;
+	var handlers = {}
+
+	handlers.onMessage = function(event) {
+		if(event.origin == origin) {
+			handlers[event.data.action].apply(window, event.data.arguments);
+		}
+		else if(event.data == "wkr_findCredentials") {
+			activeElement = document.activeElement;
+
+			var hintCreds = {};
+			if(activeElement && activeElement.form) {
+				hintCreds.username = null;
+				hintCreds.password = null;
+				for(var i = 0; i < activeElement.form.elements.length; i++) {
+					var elem = activeElement.form.elements[i];
+					if(!hintCreds.password && elem.type == "password") {
+						hintCreds.password = elem.value;
+					}
+					if((!hintCreds.username && elem.type == "text") || (elem.type == "email" && elem.value)) {
+						hintCreds.username = elem.value;
+					}
+				}
+			}
+
+			window.addEventListener("resize", function() {
+				frame.style.width = window.innerWidth + "px";
+				frame.style.height = window.innerHeight + "px";
+			}, false);
+
+			frame.addEventListener("load", function() {
+				frame.contentWindow.postMessage({"action": "getCredentials", "arguments": [hintCreds]}, autofillerUrl);
+			}, false);
+
+			frame.src = autofillerUrl + "?" + encodeURIComponent(window.location.toString());
+			frame.style.zIndex = 1000;
+			frame.style.border = "none";
+			frame.style.display = "block";
+			frame.style.position = "fixed";
+			frame.style.left = "0";
+			frame.style.top = "0";
+			frame.style.width = window.innerWidth + "px";
+			frame.style.height = window.innerHeight + "px";
+
+			document.body.appendChild(frame);
+		}
 	};
 
 	handlers.hide = function() {
@@ -67,7 +94,7 @@
 					el.value = cred.password;
 					pwd_filled = true;
 				}
-				if(el.type == "text" && user_filled == false) {
+				if((el.type == "text" || el.type == "email") && user_filled == false) {
 					el.value = cred.username;
 					user_filled = true;
 				}
@@ -96,6 +123,7 @@
 					return;
 		}
 	};
+
 
 	window.addEventListener("message", handlers.onMessage, false);
 	window.addEventListener("keypress", function(e){
